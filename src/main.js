027ce5da8d7232f23a6bedee7a27be5be044d611
Mario Araya <arayaroyo686@gmail.com>
2024-06-14 19:02:44 -0600

    Arrow Function

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 
 
-function sayHello(name = 'Mario', age = 0) {
-
-  const greeting = `Hello world function! ${name} age ${age}`
-
-  return greeting
-}
+const  sayHello = (name = 'Mario', age = 0) => `Hello world function! ${name} age ${age}`
+const add = (a = 0, b = 0) => a + b
 
 
 const result = sayHello('Mario', 22)
 
 console.log(result)
+console.log(add(10,5))
