63d161d3f56f803e68f21de3fb45e1d87381a8ed
Mario Araya <arayaroyo686@gmail.com>
2024-06-14 18:46:47 -0600

    variables and constants

---
+++ src/main.js
@@ -0,0 +1,18 @@
+
+
+let name = 'Mario'
+
+name = 'Fernando'
+
+const lastName = 'Araya'
+
+const condition = true
+
+if (condition) {
+  const lastName = 'Royo'
+  console.log(lastName)
+}
+
+
+console.log('Hello World!' + name + ' ' + lastName)
+
